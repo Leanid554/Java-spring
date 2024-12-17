a98096c4db90418bdb7fb872d4a40244ad8dc6c4
Leanid <leanid.sushchynski@gmail.com>
2024-12-17 17:25:08 +0300

    Add file test

---
+++ src/test/java/org/jl/projectmanagmentsystem/ProjectManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jl.projectmanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
